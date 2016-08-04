c10de53f89f7422fe6eb19cd74169fa088e01c84
vinay <vinayp.infanion@gmail.com>
2016-08-04 13:22:39 +0530

    3 commit

--- testpro/src/pack1/Testfirst.java
+++ testpro/src/pack1/Testfirst.java
@@ -5,5 +5,6 @@ public class Testfirst {
 	public static void main (String[] args){
 		System.out.println("Hello Don");
 		System.out.println("Hello bush");
+		System.out.println(" hello bush2");
 	}
 }
